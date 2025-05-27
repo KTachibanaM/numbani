90f67af42ed9675284eaaa2c04a2d84628540114
KTachibanaM <whj19931115@gmail.com>
2025-05-27 13:49:17 -0700

    ?

--- configuration.nix
+++ configuration.nix
@@ -101,7 +101,7 @@ in
   };
   services.cloudflared = {
     enable = true;
-  }
+  };
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
