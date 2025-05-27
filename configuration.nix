194a17619e6e73c56bc2908860ba290937f1f4fc
KTachibanaM <whj19931115@gmail.com>
2025-05-27 09:49:28 -0700

    install cloudflared

--- configuration.nix
+++ configuration.nix
@@ -99,6 +99,9 @@ in
     };
     adminCredentialsFile = "/home/${vars.username}/miniflux/admin-credentials";
   };
+  services.cloudflared = {
+    enable = true;
+  }
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
