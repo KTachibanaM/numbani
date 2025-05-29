1723af7f2ca398ec1f9f3c809716033936975aeb
KTachibanaM <whj19931115@gmail.com>
2025-05-29 13:16:29 -0700

    add rsshub

--- deploy-apps.sh
+++ deploy-apps.sh
@@ -4,3 +4,7 @@ set -e
 # miniflux
 cp /home/nixos/numbani/apps/miniflux/compose.yml /home/nixos/miniflux/compose.yml
 docker compose -f /home/nixos/miniflux/compose.yml up -d
+
+# rsshub
+cp /home/nixos/numbani/apps/rsshub/compose.yml /home/nixos/rsshub/compose.yml
+docker compose -f /home/nixos/rsshub/compose.yml up -d
