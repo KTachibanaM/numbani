abbf49594e498d0458282db9f0c47929b2993752
KTachibanaM <whj19931115@gmail.com>
2025-08-04 11:09:08 -0700

    remove weibo and telegram rss

--- deploy-apps.sh
+++ deploy-apps.sh
@@ -9,14 +9,6 @@ docker compose -f /home/nixos/miniflux/compose.yml up -d
 cp /home/nixos/nixos/apps/rsshub/compose.yml /home/nixos/rsshub/compose.yml
 docker compose -f /home/nixos/rsshub/compose.yml up -d
 
-# weibo-rss
-cp /home/nixos/nixos/apps/weibo-rss/compose.yml /home/nixos/weibo-rss/compose.yml
-docker compose -f /home/nixos/weibo-rss/compose.yml up -d
-
-# telegram-to-rss
-cp /home/nixos/nixos/apps/telegram-to-rss/compose.yml /home/nixos/telegram-to-rss/compose.yml
-docker compose -f /home/nixos/telegram-to-rss/compose.yml up -d
-
 # watchtower
 mkdir -p /home/nixos/watchtower
 cp /home/nixos/nixos/apps/watchtower/compose.yml /home/nixos/watchtower/compose.yml
